b19f2776036e69db4534826fccef099ccf3f1de4
JoacoMaurtua <joacomaurtua@gmail.com>
2021-04-03 20:33:03 -0500

    Ejercicios terminados

--- JS/functions2/script.js
+++ JS/functions2/script.js
@@ -1,4 +1,4 @@
-alert('hola');
+//alert('hola');
 
 /***********EJERCICIO 1**********/
 
@@ -22,7 +22,7 @@ console.log(a(6,8));
 
 /***********EJERCICIO 3**********/
 
-function a(x){
+/*function a(x){
   if(x[0] < x[1]) {
      return true;
   }
@@ -32,3 +32,107 @@ function a(x){
 }
 b = a([2,3,4,5])
 console.log(b);
+
+/***********EJERCICIO 4**********/
+
+/*function a(x){
+  for(var i=0; i<x.length; i++){
+    if(x[i] > 0){
+         x[i] = "Coding";
+    }
+  }
+  return x;
+}
+console.log(a([1,2,3,4]))
+
+/***********EJERCICIO 5**********/
+
+/*function a(x){
+  for(var i=0; i<x.length; i++){
+    if(x[i] > 5){
+         x[i] = "Coding";
+    }
+    else if(x[i] < 0){
+         x[i] = "Dojo";
+    }
+  }
+  return x;
+}
+console.log(a([5,7,-1,4]))
+
+/***********EJERCICIO 6**********/
+
+/*function a(x){
+  if(x[0] > x[1]) {
+   return x[1];
+  }
+  return 10;
+}
+b = a([5,10])
+console.log(b);
+
+/***********EJERCICIO 7**********/
+
+/*function sum(x){
+  sum = 0;
+  for(var i=0; i<x.length; i++){
+     sum = sum + x[i];
+     console.log(sum);
+  }
+  return sum;
+}
+a = [1,3,5,7,9,11,13,15];
+b = sum(a);
+console.log(b);
+
+
+/***********EJERCICIO 8**********/
+//Analiza los valores de un array y obtén el promedio (average) de esos valores.
+
+/*function printAverage(x){
+  sum = 0;
+  for(let i = 0; i<x.length; i++)
+  {
+    sum += x[i];
+  }
+  return sum/x.length;
+}
+
+y = printAverage([1,2,3]);
+console.log(y); // should log 2
+ 
+y = printAverage([2,5,8]);
+console.log(y); // should log 5
+
+/***********EJERCICIO 9**********/
+//Crea un array con todos los enteros impares (odd integers) entre 1 y 255 (inclusive)
+
+/*function returnOddArray(){
+  arr = [];
+  for(let i = 1; i<=255; i++)
+  {
+    if(i%2 != 0)
+    {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+y = returnOddArray();
+console.log(y); // should log [1,3,5,...,253,255]
+
+/***********EJERCICIO 10**********/
+
+function squareValue(x){
+  for(let i = 0; i<x.length; i++)
+  {
+    x[i] *=  x[i];
+  }
+  return x;
+}
+y = squareValue([1,2,3]);
+console.log(y); // should log [1,4,9]
+ 
+y = squareValue([2,5,8]);
+console.log(y); // should log [4,25,64]
+
