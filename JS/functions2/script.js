db0be903daa30509b8bcede7b4a9259df6bda446
JoacoMaurtua <joacomaurtua@gmail.com>
2021-04-03 19:14:10 -0500

    avance

---
+++ JS/functions2/script.js
@@ -0,0 +1,34 @@
+alert('hola');
+
+/***********EJERCICIO 1**********/
+
+/*function a(x,y){
+  return 5;
+}
+console.log(a(5,5))
+
+/***********EJERCICIO 2**********/
+/*function a(x,y){
+  z = []
+  z.push(x);
+  z.push(y);
+  z.push(5);
+  console.log(z);
+  return z;
+}
+b = a(2,2)
+console.log(b);
+console.log(a(6,8));
+
+/***********EJERCICIO 3**********/
+
+function a(x){
+  if(x[0] < x[1]) {
+     return true;
+  }
+  else {
+     return false;
+  }
+}
+b = a([2,3,4,5])
+console.log(b);
