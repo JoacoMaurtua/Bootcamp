edfccbfcbbcbae55374f92500531661cdf0cb28d
JoacoMaurtua <joacomaurtua@gmail.com>
2021-04-14 23:21:55 -0500

    finish

--- JS/functions4/script.js
+++ JS/functions4/script.js
@@ -1,7 +1,7 @@
 /*********Function1*********/
 /**Configura un array para que los valores negativos se transformen en 0. Por ejemplo, resetNegativos([1,2,-1,-3]) debiera dar como resultado [1,2,0,0]. */
 
-/*function convertNegatives(Arr)
+function convertNegatives(Arr)
 {
   for(let i = 0; i<Arr.length; i++){
     if(Arr[i]<0){
@@ -11,12 +11,12 @@
   return Arr;
 }
 
-console.log(convertNegatives([1,2,-1,-3]));*/
+console.log(convertNegatives([1,2,-1,-3]));
 
 /*********Function2*********/
 /*Dado un array, mueve todos los valores un espacio de derecha a izquierda eliminando el primer valor y dejando un 0 para el último valor. Por ejemplo, moverAdelante([1,2,3]) debiera dar como resultado [2,3,0].*/
 
-/*function moverAdelante(Arr){
+function moverAdelante(Arr){
   let i = 0;
   while(i<Arr.length-1){
     Arr[i] = Arr[i+1];
@@ -25,7 +25,7 @@ console.log(convertNegatives([1,2,-1,-3]));*/
   Arr[Arr.length-1] = 0;
   return Arr;
 }
-console.log(moverAdelante([1,2,3,4,5,6,7]));*/
+console.log(moverAdelante([1,2,3,4,5,6,7]));
 
 /*********Function3*********/
 /** Configura un array para que el resultado sean los valores en el orden contrario. Por ejemplo, returnReverso([1,2,3]) debe dar [3,2,1].*/
@@ -39,10 +39,30 @@ function swap(a,b)
 function returnReverso(Arr)
 {
   for(let i = 0; i<Arr.length/2; i++){
-    swap(Arr[i],Arr[Arr.length-i-1]);
+    //swap(Arr[i],Arr[Arr.length-i-1]); 
+      let temp = Arr[i];
+      Arr[i] = Arr[Arr.length-i-1];
+      Arr[Arr.length-i-1] = temp;
   }
   return Arr;
 }
 
 let x = returnReverso([1,2,3,4]);
 console.log(x);
+
+/*********Function4*********/
+/** Crea una función que cambie un array repitiendo sus valores originales (manteniendo el mismo orden). Por ejemplo, repetirValores([4,”Ulysses”, 42, false]) debiera dar [4,4, “Ulysses”, “Ulysses”, 42, 42, false, false].*/
+
+function repetirValores(Arr)
+{
+  for(let i = Arr.length-1; i>=0; i--)
+  {
+    Arr[2*i+1] = Arr[i];
+    Arr[2*i] = Arr[i]
+                                                                                   
+  }
+  console.log(Arr);
+}
+
+repetirValores([4,"Ulyses",42,false]);
+
